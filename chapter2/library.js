b4a897454c37a294994f041e0e736a299d39f1be
issei1213 <59830008+issei1213@users.noreply.github.com>
2024-03-08 15:39:51 +0900

    complete chapter2

---
+++ chapter2/library.js
@@ -0,0 +1,41 @@
+class Library {
+    static getBookLendings(libraryData, userId, memberId) {
+        if(UserManagement.isLibrarian(libraryData.userManagement, userId) || UserManagement.isSuperMember(libraryData.userManagement, userId)) {
+            return Catalog.getBookLendings(libraryData.catalog, memberId)
+        } else {
+            throw new Error("You are not authorized to perform this operation")
+        }
+    }
+
+    static addBookItem(libraryData, userId, bookItem) {
+        if(UserManagement.isLibrarian(libraryData.userManagement, userId) || UserManagement.isVipMember(libraryData.userManagement, userId)) {
+            Catalog.addBookItem(libraryData.catalog, bookItem)
+        } else {
+            throw new Error("You are not authorized to perform this operation")
+        }
+    }
+}
+
+class UserManagement {
+    static isLibrarian(userManagementData, userId) {
+        //...
+    }
+
+    static isSuperMember(userManagementData, userId) {
+        //...
+    }
+
+    static isVipMember(userManagementData, userId) {
+        //...
+    }
+}
+
+class Catalog {
+    static getBookLendings(catalogData, memberId) {
+        //...
+    }
+
+    static addBookItem(catalogData, bookItem) {
+        //...
+    }
+}
